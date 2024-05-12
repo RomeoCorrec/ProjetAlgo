8f4aacaf8499982c81e3dc6c178db409bfcea992
Mathischmp <mathischampagne@yahoo.fr>
2024-05-12 14:59:16 +0200

    ca marche pas lol

---
+++ app/app/forms/creationAccount.py
@@ -0,0 +1,12 @@
+# app/forms.py
+from django import forms
+
+class UserCreationForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    name = forms.CharField(max_length=100)
+    surname = forms.CharField(max_length=100)
+    age = forms.IntegerField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    location = forms.CharField(max_length=100)
+    sex = forms.CharField(max_length=10)
+    mail = forms.EmailField()
