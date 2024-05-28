c5a89a9d4843a2d03cc21ee50047ad0b888aa3b7
Romeo <romeo.correc@gmail.com>
2024-05-28 12:09:00 +0200

    test

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from ProjetAlgo.app.app.Class.User import User
 from ProjetAlgo.app.app.utils import graphDB
 
-GDB = graphDB("bolt://localhost:7687", "neo4j", "password")
+GDB = graphDB("neo4j+s://fb7779f4.databases.neo4j.io", "neo4j", "4DTufAW-6wB0UlMjaLpJ_53j1-ZzKV3N1U3tCCka9Qo")
 
-print(GDB.get_filtered_posts("ROMEO", "image"))
+user = User("user2", "user", "user", 22, "Password@123", "Paris", "M", "mail@mail.com")
+user.profile_picture = None
+query = """Create (u:User {username: $username, name: $name, surname: $surname, age: $age,
+ password: $password, location: $location, sex: $sex, mail: $mail, private: false, profile_picture: $profile_picture})"""
+
+GDB._driver.session().run(query, username=user.username, name=user.name, surname=user.surname, age=user.age, password=user.password, location=user.location,
+                          sex=user.sex, mail=user.mail, profile_picture=user.profile_picture)
